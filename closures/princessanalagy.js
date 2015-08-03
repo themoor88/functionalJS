a2806d74d6e98aeed4fa5e41db5a4adc51aefcd2
Wajid Kagzi <wjkagzi@gmail.com>
2015-08-03 11:30:02 -0400

    Added princess analogy for closures.

---
+++ closures/princessanalagy.js
@@ -0,0 +1,19 @@
+function princess() {
+  var adventures = [];
+
+  function princeCharming() {}
+
+  var unicorn = {};
+  var dragons = [];
+  var squirrel = "Hello!";
+
+  return {
+    story: function() {
+      return adventures[adventures.length -1];
+    }
+  }
+}
+
+var littleGirl = princess();
+
+littleGirl.story();
